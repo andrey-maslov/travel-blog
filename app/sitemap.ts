6d2d81fcf28ffaba79af548ef78612547ff518de
andrei.maslau <andrei.maslau@olx.pl>
2025-06-13 11:48:01 +0200

    sitemap added

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://blog.tripplanr.io',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    // {
+    //   url: 'https://blog.tripplanr.io/about',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.7,
+    // },
+    // {
+    //   url: 'https://blog.tripplanr.io/posts',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'weekly',
+    //   priority: 0.6,
+    // },
+    {
+      url: 'https://blog.tripplanr.io/posts/pervyj-post',
+      lastModified: new Date('2025-06-06'),
+      changeFrequency: 'monthly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://blog.tripplanr.io/posts/kozanov-mikroraion-na-okraine-wroclava',
+      lastModified: new Date('2025-06-06'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
